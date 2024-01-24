7d9fc05e730583a035ffd07cef74bf15c9659bde
Alonso Valdes <alonsoo8.5@gmail.com>
2024-01-24 12:16:47 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+
+
+print('Hello world')
+print('This is the main file, and will be uploaded to github')
+
+print('Por favor, ingresa tu nombre como print en la lista y realiza un commit.')
+
+lista_commits = []
+
+print(f'Los que han realizado un commit son: {', '.join(lista_commits)}')
